2979eb70cf68a93ce87e5c7f08b21368a22e5c1c
rahuliyer95 <rahuliyer573@gmail.com>
2014-06-06 14:30:31 +0530

    Added Push Notification Storage

--- app/src/main/java/com/vjti/fests/Utils.java
+++ app/src/main/java/com/vjti/fests/Utils.java
@@ -3,6 +3,26 @@ package com.vjti.fests;
 import android.app.Activity;
 import android.content.Context;
 
+import com.vjti.fests.model.PushMessage;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
 
@@ -87,6 +107,102 @@ public class Utils {
 
     private static final void crouton (Activity activity, String message, Style style) {
         Crouton.makeText(activity, message, style).show();
+    }
+
+    /**
+     * Xml parsing
+     */
+
+    public final static String ENTHUSIA = "enthusia.xml";
+    public final static String PRATIBIMB = "pratibimb.xml";
+    public final static String TECHNOVANZA = "technovanza.xml";
+
+    /**
+     * Use this method to get all the push notifications received so far
+     * @param context Context of the android application running (getApplicationContext()) will help
+     * @param file Use the file for your particular festival e.g: Utils.ENTHUSIA, Utils.PRATIBIMB, Utils.TECHNOVANZA
+     * @return a list of PushMessage objects containing respective data
+     * @throws java.io.IOException Throws the IOException occurred in the read process
+     */
+
+    public final static ArrayList<PushMessage> getPushMessages (Context context, String file) throws  IOException {
+        if (new File(context.getCacheDir(), file).exists()) {
+            return getPushMessage(new File(context.getCacheDir(), file));
+        }
+        return null;
+    }
+
+    private final static ArrayList<PushMessage> getPushMessage (File file) throws IOException {
+        ArrayList<PushMessage> messages = new ArrayList<PushMessage>();
+        try {
+            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
+            NodeList elements = document.getElementsByTagName("data");
+            for (int i=0; i < elements.getLength(); i++) {
+                if (elements.item(i).getNodeType() == Node.ELEMENT_NODE)  {
+                    messages.add( new PushMessage(
+                            ((Element) elements.item(i)).getElementsByTagName("message").item(0).getTextContent(),
+                            Boolean.valueOf(((Element) elements.item(i)).getElementsByTagName("read").item(0).getTextContent())
+                    ));
+                }
+            }
+            document = null;
+            elements = null;
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace();
+        } catch (SAXException ex) {
+            ex.printStackTrace();
+        }
+        return messages;
+    }
+
+    /**
+     * Use this method to store the push messages to file
+     * @param context Context of the android application running (getApplicationContext()) will help
+     * @param file Use the file for your particular festival e.g: Utils.ENTHUSIA, Utils.PRATIBIMB, Utils.TECHNOVANZA
+     * @param messages The List of message you have
+     * @throws java.io.IOException Throws the IOException that occurred during the write process
+     */
+
+    public static final void editPushMessages(Context context, String file, ArrayList<PushMessage> messages) throws  IOException {
+        editPushMessages(new File(context.getCacheDir(), file), messages);
+    }
+
+    private static final void editPushMessages(File file, ArrayList<PushMessage> messages) throws  IOException {
+
+        try {
+            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+
+            Element root = document.createElement("notification");
+            document.appendChild(root);
+
+            for (PushMessage message : messages) {
+
+                Element data, content, read;
+
+                data = document.createElement("data");
+                content = document.createElement("message");
+                read = document.createElement("read");
+
+                content.appendChild(document.createTextNode(message.getMessage()));
+                read.appendChild(document.createTextNode(String.valueOf(message.isRead()).toLowerCase()));
+
+                data.appendChild(content);
+                data.appendChild(read);
+
+                root.appendChild(data);
+            }
+
+            TransformerFactory.newInstance().newTransformer().transform(new DOMSource(document), new StreamResult(file));
+
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace();;
+        } catch (TransformerConfigurationException ex) {
+            ex.printStackTrace();
+        } catch (TransformerException ex) {
+            ex.printStackTrace();
+        }
+
+
     }
 
 }
